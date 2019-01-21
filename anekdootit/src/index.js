8f2a0e2d35b4f909a83fd829f95d11a30fb98c19
Tuukka Turunen <tuukka.turunen@helsinki.fi>
2019-01-21 15:45:42 +0200

    spaces fixed

--- anekdootit/src/index.js
+++ anekdootit/src/index.js
@@ -2,9 +2,9 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const Header = (props) => {
-	return(
-		<h1>{props.text}</h1>
-	)
+  return (
+    <h1>{props.text}</h1>
+  )
 }
 
 const Button = (props) =>
@@ -16,30 +16,29 @@ const App = (props) => {
 
     const numb = Math.floor(Math.random() * props.anecdotes.length)
     setSelected(numb)
-    console.log(numb)
   }
-  const init = Array.apply(null, new Array(props.anecdotes.length)).map(Number.prototype.valueOf,0)
+  const init = Array.apply(null, new Array(props.anecdotes.length)).map(Number.prototype.valueOf, 0)
   const [points, setPoints] = useState(init)
   const vote = () => {
     const copy = [...points]
-    copy[selected] +=1
-    setPoints(copy)  
-	}
-	
+    copy[selected] += 1
+    setPoints(copy)
+  }
 
-	const mostVotedIndex = points.indexOf(Math.max(...points))
+
+  const mostVotedIndex = points.indexOf(Math.max(...points))
 
 
   return (
     <div>
-			<Header text='Anecdote of the day'/>
+      <Header text='Anecdote of the day' />
       <p>{props.anecdotes[selected]}</p>
       <p>has {points[selected]} votes</p>
-      <Button handleClick={vote} text='vote'/>
-      <Button handleClick={rand} text='next anecdote'/>
-      <Header text='Anecdote with most votes'/>
-			<p>{props.anecdotes[mostVotedIndex]}</p>
-			<p>has {points[mostVotedIndex]} votes</p>
+      <Button handleClick={vote} text='vote' />
+      <Button handleClick={rand} text='next anecdote' />
+      <Header text='Anecdote with most votes' />
+      <p>{props.anecdotes[mostVotedIndex]}</p>
+      <p>has {points[mostVotedIndex]} votes</p>
     </div>
   )
 }
